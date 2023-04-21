6b0558e0d1a63b06eabca7053b19c4ff03eef27d
sofiafernands <fernandesofia1802@gmail.com>
2023-04-21 19:36:49 +0000

    my todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+function TodoList() {
+	//se declaran las listas en un array 
+  const [todos, setTodos] = useState([]);
+
+  // esta funcion es para agregar tarea
+  const handleAddTask = (ev) => {
+	//propiedad que se coloca para evitar el evento por defecto.(preventDefault)
+    ev.preventDefault();
+ 
+    const newTask = ev.target.todo.value;
+	//aqui hacemos un if para que cuando se escriba algo en el input se agregue como 
+	//una nueva tarea.
+    if (newTask !== "") {
+      setTodos([...todos, newTask]);
+      ev.target.todo.value = "";
+    }
+  };
+
+  const deleteTask = (todos.filter((todos,index,arr)) => { arr.pop();
+return todos.length === "")};
+
+	return(value ==="" )
+	console.log(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleAddTask}>
+        <input type="text"
+		 name="todo"
+		  placeholder="Add new task" />
+
+        <button type="submit">Add New</button>
+
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} onClick={()=>deleteTask(todo)} > {todo} </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+
+}
+
+export default TodoList;
+
+
+
+
+
+
+
+
